4bd7ccc14650e83c8794852a455efc7ab53b27ce
moheng <ylm200322400@163.com>
2020-07-14 16:00:55 +0800

    更新样板

---
+++ src/json/daan.e.ts
@@ -0,0 +1,10 @@
+import { Itim } from "../dt";
+
+export const di2:Itim[] = [
+    {
+        question: 1,
+        xuez: [
+            "C"
+        ]
+    }
+]
---
+++ src/json/student.e.ts
@@ -0,0 +1,5 @@
+import { Istudent } from '../dt';
+
+export let Studentlist: Istudent[] = [
+    {"xm":"***","xj":"G320826200208050004"},
+]
